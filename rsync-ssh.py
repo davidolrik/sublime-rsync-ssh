4bca02986eef89c37e8e32a6198030e8a24461be
Vasily Asakasinsky <asakasinsky@gmail.com>
2014-12-03 20:09:12 +0300

    small fix

--- rsync-ssh.py
+++ rsync-ssh.py
@@ -211,7 +211,7 @@ class Rsync(threading.Thread):
                 timeout=self.timeout,
                 stderr=subprocess.STDOUT
             ).decode('utf-8')
-            if not re.match("bash: type: rsync:", output):
+            if re.match("bash: type: rsync:", output):
                 message = "ERROR: Unable to locate rsync on "+self.remote.get("remote_host")
                 console_print(self.remote.get("remote_host"), self.local_path, message)
                 sublime.active_window().run_command("terminal_notifier", {
